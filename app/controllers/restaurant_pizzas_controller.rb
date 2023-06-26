19f0f4a908b802f67ffe33a4197dea5b43ca2c5f
Yegon Japheth Kiptoo <jyegon504@gmail.com>
2023-06-26 12:57:37 +0300

    Completed restaurant_pizza controller methods

--- app/controllers/restaurant_pizzas_controller.rb
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -1,4 +1,16 @@
 class RestaurantPizzasController < ApplicationController
     def create
+      restaurant_pizza = RestaurantPizza.new(restaurant_pizza_params)
+      if restaurant_pizza.save
+        render json: restaurant_pizza.pizza, only: [:id, :name, :ingredients]
+      else
+        render json: { errors: restaurant_pizza.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+  
+    private
+  
+    def restaurant_pizza_params
+      params.permit(:price, :pizza_id, :restaurant_id)
     end
 end
